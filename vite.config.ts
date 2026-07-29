1bf21f4ca8194ff718af9071bb9e85939578850c
vladimir <vladimir.rdguez@gmail.com>
2026-07-29 12:01:24 -0400

    Allow cloflared tunnels

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import adapter from '@sveltejs/adapter-static';
 import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
+	server: {
+        allowedHosts: true
+	},
 	plugins: [
 		tailwindcss(),
 		sveltekit({
